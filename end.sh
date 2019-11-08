0c87b7984d632b311c8598bf378f106770f5be64
Ian Murphy <ian@rockfortengineering.com>
2019-11-08 16:03:36 +0000

    Script updates.
    Script updates.

--- end.sh
+++ end.sh
@@ -1,3 +1,8 @@
+# First bring down the CAN interfaces
+sudo ip link set down emuccan0
+sudo ip link set down emuccan1
+
+# Kill the daemon & driver
 sudo pkill -f emucd
 sleep 0.1
 sudo rmmod emuccan.ko
--- start.sh
+++ start.sh
@@ -1,20 +1,26 @@
-# Kills the previously running driver
+# Script edited 2019-11-08 by Ian Murphy
+
+cd # ensure we are 'home', we expect a sub-folder 'emuc-b201'
+
+# Kills the daemon & driver if already running
 sudo pkill -f emucd
 sleep 0.1
 sudo rmmod emuccan.ko
-sudo insmod src/emuccan.ko
 
-# Start 2 CAN channels
-# -sN option sets the bitrate
-#       -s4  100 KBPS
-#       -s5  125 KBPS
-#       -s6  250 KBPS
-#       -s7  500 KBPS
-#       -s8  800 KBPS
-#       -s9  1 MBPS 
-sudo src/emucd -s7 ttyACM0 can0 can1
-sudo sudo ip link set can0 up qlen 10
-sudo sudo ip link set can1 up qlen 10
+# Install the kernel module
+sudo insmod ./emuc-b201/src/emuccan.ko
 
+# Start daemon and specify CAN interfaces
+# -sN option sets the bitrate NOTE: PDF & command line are correct, previous script was not...
+#       -s3  50 KBPS
+#       -s4  125 KBPS
+#       -s5  250 KBPS
+#       -s6  500 KBPS
+#       -s7  1 MBPS 
+sudo ./emuc-b201/src/emucd -s6 ttyACM0 emuccan0 emuccan1 # use default names to avoid conflict with installed PEAK hardware
+sudo ip link set up emuccan0 qlen 10
+sudo ip link set up emuccan1 qlen 10
+
+# NOTE: the following statement needs validating
 # Note about qlen: It was found that 10 works
 # better than the default of 10000
